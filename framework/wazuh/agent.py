1aca8d27b9dd3d29ca10e5c09335acaaae137e79
BraulioV <braulio@wazuh.com>
2018-06-27 16:49:45 -0700

    Add support for unified WPK for Wazuh 3.4.0 and previous versions references #864
    The `agent_upgrade` now can upgrade an agent using the unified WPK and also support the downgrade to previous versions using the previous WPKs.

--- framework/wazuh/agent.py
+++ framework/wazuh/agent.py
@@ -1896,14 +1896,16 @@ class Agent:
         return data
 
 
-    def _get_versions(self, wpk_repo=common.wpk_repo_url):
+    def _get_versions(self, wpk_repo=common.wpk_repo_url, desired_version=None):
         """
         Generates a list of available versions for its distribution and version.
         """
-        if self.os['platform']=="windows":
-            versions_url = wpk_repo + "windows/versions"
+        if desired_version is None or desired_version[:4] >= "v3.4" and self.os['platform'] != "windows":
+            versions_url = wpk_repo + "linux/" + self.os['arch'] + "/versions"
         else:
-            if self.os['platform']=="ubuntu":
+            if self.os['platform']=="windows":
+                versions_url = wpk_repo + "windows/versions"
+            elif self.os['platform']=="ubuntu":
                 versions_url = wpk_repo + self.os['platform'] + "/" + self.os['major'] + "." + self.os['minor'] + "/" + self.os['arch'] + "/versions"
             else:
                 versions_url = wpk_repo + self.os['platform'] + "/" + self.os['major'] + "/" + self.os['arch'] + "/versions"
@@ -1937,12 +1939,12 @@ class Agent:
         Searchs latest Wazuh WPK file for its distribution and version. Downloads the WPK if it is not in the upgrade folder.
         """
         agent_new_ver = None
-        if not version:
-            versions = self._get_versions(wpk_repo)
+        if version is None:
+            versions = self._get_versions(wpk_repo, version)
             agent_new_ver = versions[0][0]
             agent_new_shasum = versions[0][1]
         else:
-            for versions in self._get_versions(wpk_repo):
+            for versions in self._get_versions(wpk_repo, version):
                 if versions[0] == version:
                     agent_new_ver = versions[0]
                     agent_new_shasum = versions[1]
@@ -1972,11 +1974,20 @@ class Agent:
         # Generating file name
         if self.os['platform']=="windows":
             wpk_file = "wazuh_agent_{0}_{1}.wpk".format(agent_new_ver, self.os['platform'])
+            wpk_url = wpk_repo + "windows/" + wpk_file
+            
         else:
-            if self.os['platform']=="ubuntu":
-                wpk_file = "wazuh_agent_{0}_{1}_{2}.{3}_{4}.wpk".format(agent_new_ver, self.os['platform'], self.os['major'], self.os['minor'], self.os['arch'])
+            if version is None or version[:4] >= "v3.4":
+                wpk_file = "wazuh_agent_{0}_linux_{1}.wpk".format(agent_new_ver, self.os['arch'])
+                wpk_url = wpk_repo + "linux/" + self.os['arch'] + "/" + wpk_file
+
             else:
-                wpk_file = "wazuh_agent_{0}_{1}_{2}_{3}.wpk".format(agent_new_ver, self.os['platform'], self.os['major'], self.os['arch'])
+                if self.os['platform']=="ubuntu":
+                    wpk_file = "wazuh_agent_{0}_{1}_{2}.{3}_{4}.wpk".format(agent_new_ver, self.os['platform'], self.os['major'], self.os['minor'], self.os['arch'])
+                    wpk_url = wpk_repo + self.os['platform'] + "/" + self.os['major'] + "." + self.os['minor'] + "/" + self.os['arch'] + "/" + wpk_file
+                else:
+                    wpk_file = "wazuh_agent_{0}_{1}_{2}_{3}.wpk".format(agent_new_ver, self.os['platform'], self.os['major'], self.os['arch'])
+                    wpk_url = wpk_repo + self.os['platform'] + "/" + self.os['major'] + "/" + self.os['arch'] + "/" + wpk_file
 
         wpk_file_path = "{0}/var/upgrade/{1}".format(common.ossec_path, wpk_file)
 
@@ -1994,14 +2005,6 @@ class Agent:
                 return [wpk_file, sha1hash]
 
         # Download WPK file
-        if self.os['platform']=="windows":
-            wpk_url = wpk_repo + "windows/" + wpk_file
-        else:
-            if self.os['platform']=="ubuntu":
-                wpk_url = wpk_repo + self.os['platform'] + "/" + self.os['major'] + "." + self.os['minor'] + "/" + self.os['arch'] + "/" + wpk_file
-            else:
-                wpk_url = wpk_repo + self.os['platform'] + "/" + self.os['major'] + "/" + self.os['arch'] + "/" + wpk_file
-
         if debug:
             print("Downloading WPK file from: {0}".format(wpk_url))
 
